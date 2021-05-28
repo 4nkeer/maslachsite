b4676b1017b67fcf7e1e7f7cdafd3c3862b0d90a
Anton <anton.kerentsev@gmail.com>
2021-05-28 11:10:50 +0600

    redesign

--- js/main.js
+++ js/main.js
@@ -1,21 +1,11 @@
-jQuery(function($) {
-    //подвешиваем на событие клик по кнопке
-    $('.btn').on('click', function(e) {
-      //отключение ее
-      $(e.currentTarget).attr('disabled', true);
-      // показываем процесс закгрузки
-      $('.loader').toggleClass('hide');
-      $('.btn').toggleClass('hide');
-      $('p').toggleClass('hide');
 
-      // подвешиваем паузу на 5 секунд
-      setTimeout(function() {
-        // скрываем процесс загрузки
-        $('.loader').toggleClass('hide');
-        // делаем кнопку активной
-        $(e.currentTarget).attr('disabled', false);
-        $('.btn').removeClass('hide')
-        $('p').toggleClass()
-      }, 5000)
-    });
-  });
+    $('#headd').hover(onIn, onOut);
+
+
+function onIn() {
+    $('.heading font').css("bacground", "green")
+}
+
+function onOut() {
+    $('#descr').toggleClass('')
+}
