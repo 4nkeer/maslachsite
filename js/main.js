670789251ee6fb71ab6418d7ff55ecba01ddf866
Anton <anton.kerentsev@gmail.com>
2021-05-28 14:40:21 +0600

    add .content

--- js/main.js
+++ js/main.js
@@ -1,17 +1,12 @@
 
-    $('#headd').hover(onIn, onOut);
-
-
-function onIn() {
-    $('.heading font').css("bacground", "green")
-}
-
-function onOut() {
-    $('#descr').toggleClass('')
-}
-
 $(window).on('load', function () {
     
         $('#load').delay(2500).fadeOut('slow');
     
+    });
+
+
+$('#games').click(function () {
+    $('.contenthide').toggleClass('content')
+    $('.contenthide').load('./content/content.html');
     });
